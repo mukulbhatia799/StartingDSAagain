e786373bdf540f992ecaf79a909de628d0835aa1
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-09-19 20:27:42 +0530

    patterns 5th problem solved

--- patterns.cpp
+++ patterns.cpp
@@ -5,13 +5,11 @@ int main(){
     int n;
     cin >> n;
 
-    int x = 1;
-    for(int i = 0; i < n; i++)
+    for(int i = 1; i <= n; i++)
     {
-        for(int j = 0; j <= i; j++)
+        for(int j = 0; j < i; j++)
         {
-            cout << x << " ";
-            x++;
+            cout << i+j << " ";
         }
         cout << endl;
     }
