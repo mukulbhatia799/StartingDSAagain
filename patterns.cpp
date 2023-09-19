6d82e28d80c493ecc7574fa97ea5b6bd7af9454e
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-09-19 20:18:50 +0530

    pattern 3rd problem done

--- patterns.cpp
+++ patterns.cpp
@@ -7,9 +7,9 @@ int main(){
 
     for(int i = 0; i < n; i++)
     {
-        for(int j = 0; j < n; j++)
+        for(int j = 0; j <= i; j++)
         {
-            cout << "* ";
+            cout << "*";
         }
         cout << endl;
     }
