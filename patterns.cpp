c0953b440f103cbbdd77aeaead4b4a2a2b60063c
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-09-19 20:42:23 +0530

    pattern 8th problem solved

--- patterns.cpp
+++ patterns.cpp
@@ -5,12 +5,13 @@ int main(){
     int n;
     cin >> n;
 
-    for(int i = 0; i < n; i++)
+    for(int i = 1; i <= n; i++)
     {
         char c = 'A';
         for(int j = 1; j <= n; j++)
         {
-            cout << char(c + i) << " ";
+            char ch = c + j + i - 2;
+            cout << ch << " ";
         }
         cout << endl;
     }
