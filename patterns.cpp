c1c9b8a618340d262d99a128be3e78f0a86e47c5
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-09-19 20:51:28 +0530

    patterns 10th solved

--- patterns.cpp
+++ patterns.cpp
@@ -7,10 +7,14 @@ int main(){
 
     for(int i = 1; i <= n; i++)
     {
-        char ch = 'A';
+        // spaces
+        for(int j = 1; j <= n-i; j++)
+        {
+            cout << "  ";
+        }
         for(int j = 1; j <= i; j++)
         {
-            cout << char(ch + n - j) << " ";
+            cout << "* ";
         }
         cout << endl;
     }
