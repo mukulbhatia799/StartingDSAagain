870f14ff3590792f7a895970af7d6754a3e3eb7e
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-09-19 20:54:10 +0530

    patterns 11th solved

--- patterns.cpp
+++ patterns.cpp
@@ -7,12 +7,7 @@ int main(){
 
     for(int i = 1; i <= n; i++)
     {
-        // spaces
-        for(int j = 1; j <= n-i; j++)
-        {
-            cout << "  ";
-        }
-        for(int j = 1; j <= i; j++)
+        for(int j = i; j <= n; j++)
         {
             cout << "* ";
         }
