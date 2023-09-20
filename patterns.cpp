b48e7e875dd505fbf92d61ef8be130e6e5aced05
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-09-20 13:12:03 +0530

    patterns solved 14th

--- patterns.cpp
+++ patterns.cpp
@@ -7,21 +7,16 @@ int main(){
 
     for(int i = 1; i <= n; i++)
     {
-        for(int j = i; j <= n; j++)
+        for(int j = 1; j < i; j++)
         {
-            cout << j-i+1 << " ";
+            cout << " ";
         }
-        for(int j = 1; j <= 2*i-2; j++)
+        for(int j = i; j <= n; j++)
         {
             cout << "* ";
         }
-        for(int j = 1; j <= n-i+1; j++)
-        {
-            cout << n-j-i+2 << " ";
-        }
         cout << endl;
     }
-
 
     return 0;
 }
