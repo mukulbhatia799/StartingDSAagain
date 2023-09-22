1134f2267466f8db5f8071e1fe94c6842b9ab50b
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-09-22 17:06:32 +0530

    patterns half pyramid solved

--- patterns.cpp
+++ patterns.cpp
@@ -7,11 +7,9 @@ int main(){
 
     for(int i = 1; i <= n; i++)
     {
-        for(int j = 1; j <= n*2-1; j++)
+        for(int j = 1; j <= i; j++)
         {
-            if((i == 1 || j == 1) || (i == n || j == n*2-1))
-                cout << "* ";
-            else cout << "  ";
+            cout << "* ";
         }
         cout << endl;
     }
