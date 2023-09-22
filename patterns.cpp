e2198c880dd54c3229ee3063e0be951aaac9c059
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-09-22 17:04:43 +0530

    solved hollow rectangle ques of patterns

--- patterns.cpp
+++ patterns.cpp
@@ -7,13 +7,11 @@ int main(){
 
     for(int i = 1; i <= n; i++)
     {
-        for(int j = 1; j <= n-i; j++)
+        for(int j = 1; j <= n*2-1; j++)
         {
-            cout << "  ";
-        }
-        for(int j = 1; j <= i; j++)
-        {
-            cout << i+j-1 << " ";
+            if((i == 1 || j == 1) || (i == n || j == n*2-1))
+                cout << "* ";
+            else cout << "  ";
         }
         cout << endl;
     }
