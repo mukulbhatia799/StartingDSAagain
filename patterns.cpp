d985a724a2053dd0bf14f80f112a5b349e23ecb2
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-09-22 18:09:16 +0530

    solved full pyramid pattern problem

--- patterns.cpp
+++ patterns.cpp
@@ -5,13 +5,15 @@ int main(){
     int n;
     cin >> n;
 
-    for(int i = 1; i <= n; i++) // i = 2
+    for(int i = 1; i <= n; i++)
     {
-        for(int j = 1; j <= n; j++) // j = 2
+        for(int j = 1; j < i; j++)
         {
-            if(i == 1 || j == 1 || j == n-i+1)
-                cout << "* ";
-            else cout << "  ";
+            cout << " ";
+        }
+        for(int j = 1; j <= n-i+1; j++)
+        {
+            cout << "* ";
         }
         cout << endl;
     }
