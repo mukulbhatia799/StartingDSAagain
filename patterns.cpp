8cc84a7126cb07af107ced7f2c9941c7a83e3010
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-09-23 23:10:03 +0530

    solving pepcoding ques

--- patterns.cpp
+++ patterns.cpp
@@ -7,11 +7,7 @@ int main(){
 
     for(int i = 1; i <= n; i++)
     {
-        for(int j = 1; j < i; j++)
-        {
-            cout << " ";
-        }
-        for(int j = 1; j <= n-i+1; j++)
+        for(int j = 1; j <= i; j++)
         {
             cout << "* ";
         }
