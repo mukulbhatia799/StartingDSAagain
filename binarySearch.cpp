e1642be17b7b94545d85f35d66ee8f341ba163d7
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-09-24 19:16:15 +0530

    binary search: changed mid formula when the max int range is crossed

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -6,7 +6,14 @@ int binarySearch(int arr[], int n, int findele)
     int low = 0, high = n-1;
     while(low <= high)
     {
-        int mid = (low + high) / 2;
+        // int mid = (low + high) / 2;
+        int mid = low + (high-low)/2;
+        /*
+            As int max value is 2^31-1. so, low and high both are int and suppose the low and high so adding 
+            them can be out of range to store in mid. So, 
+            we can use some maths tricks like (low + (high-low)/2) is same as (low+high)/2.
+        */ 
+       
         if(arr[mid] == findele) return mid;
         else if(arr[mid] < findele) low = mid + 1;
         else high = mid - 1;
