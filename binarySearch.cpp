e4552e76e1bef8ab0512e21cd299fb4e1e06d687
Mukul Bhatia <mukulbhatia189@gmail.com>
2023-09-24 22:02:33 +0530

    find first and last occ. of an element in a sorted array using binary search solved!

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,36 +1,61 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int binarySearch(int arr[], int n, int findele)
+int firstOccOfK(int arr[], int n, int k)
 {
-    int low = 0, high = n-1;
+    int low = 0, high = n-1, mid = 0, ans = -1;
+
     while(low <= high)
     {
-        // int mid = (low + high) / 2;
-        int mid = low + (high-low)/2;
-        /*
-            As int max value is 2^31-1. so, low and high both are int and suppose the low and high so adding 
-            them can be out of range to store in mid. So, 
-            we can use some maths tricks like (low + (high-low)/2) is same as (low+high)/2.
-        */ 
-       
-        if(arr[mid] == findele) return mid;
-        else if(arr[mid] < findele) low = mid + 1;
-        else high = mid - 1;
+        mid = (low + high) / 2;
+
+        if(arr[mid] < k) low = mid + 1;
+        else if(arr[mid] > k) high = mid - 1;
+        else 
+        {
+            ans = mid;
+            high = mid-1;
+        }
     }
-    return -1;
+
+    return ans;
 }
 
+int lastOccOfK(int arr[], int n, int k)
+{
+    int low = 0, high = n-1, ans = -1;
+
+    while(low <= high)
+    {
+        int mid = (low + high) / 2;
+
+        if(arr[mid] < k) low = mid + 1;
+        else if(arr[mid] > k) high = mid - 1;
+        else 
+        {
+            ans = mid;
+            low = mid + 1;
+        }
+    }
+
+    return ans;
+}
+
+
+
 int main(){
-    int n, findele;
-    cin >> n >> findele;
+    int n, k;
+    cin >> n >> k;
 
     int arr[n];
+
     for(int i = 0; i < n; i++) cin >> arr[i];
 
-    int index = binarySearch(arr, n, findele);
-    if(index == -1) cout << "Value not found" << endl;
-    else cout << "Value found at " << index << " index." << endl; 
+    int focc = firstOccOfK(arr, n, k);
+    int locc = lastOccOfK(arr, n, k);
+
+    cout << focc << " " << locc << endl;
+
 
 
     return 0;
